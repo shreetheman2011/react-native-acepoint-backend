6a7de26351a8dc02ebef12cc1f51047a18a07972
shree manick <shree.manickaraja@gmail.com>
2025-06-10 13:22:54 -0700

    change auth middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import User from "../models/User.js";
 const protectRoute = async (req, res, next) => {
   try {
     //get token
-    const token = req.header("Authorization").replace("Bearer", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "").trim();
     if (!token)
       return res
         .status(401)
